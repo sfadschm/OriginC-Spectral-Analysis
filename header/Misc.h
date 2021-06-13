da87c10b076b0214886ff4a44344eeb67158874f
Alex Schmitz <alexander.schmitz@uni-due.de>
2021-06-13 23:44:58 +0200

    Add timer function for debugging.

--- header/Misc.h
+++ header/Misc.h
@@ -13,6 +13,7 @@
  * This file provides general helper methods unrelated to Origin objects.
  */
 #include <Origin.h>
+#include <time.h>
 
 /**
  * Remove special characters and other stuff from a given string to serve as a name.
@@ -80,6 +81,46 @@ string MISC_formatString(string base, string str1 = "", string str2 = "", string
 	result.Format(base, str1, str2, str3);
 
 	return result;
+}
+
+/**
+ * Global storage for the timer
+ */
+int lastClock = 0;
+
+/**
+ * Output a timer from the last time the method was called.
+ *
+ * @param string label if empty, resets the global timer
+ */
+void timer(string label = "")
+{
+	// Get system time
+	struct tm the2Time ;
+	SYSTEMTIME sys2Time ;
+	GetSystemTime(&sys2Time);
+
+	// Convert to ms value
+	int milliTime = ((((sys2Time.wDay) * 24 + sys2Time.wHour) * 60 + sys2Time.wMinute) * 60 + sys2Time.wSecond) * 1000 + sys2Time.wMilliseconds; 
+
+	// No output without label
+	if(label != "")
+	{
+		// Output time since last call
+		if(lastClock == 0)
+		{
+			printf("%s: %d ms\n", label, 0);
+		}
+		else
+		{
+			printf("%s: %d ms\n", label, (milliTime - lastClock));
+		}
+	}
+	
+	// Set new clock
+	lastClock = milliTime;
+	
+	return;
 }
 
 #endif
