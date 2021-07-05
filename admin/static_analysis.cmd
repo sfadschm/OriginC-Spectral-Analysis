8a254ab1821ab8a5602b88b2fe877a9098bc424b
Alex Schmitz <alexander.schmitz@uni-due.de>
2021-07-05 22:43:28 +0200

    Add cppcheck to static analysis.

--- admin/static_analysis.cmd
+++ admin/static_analysis.cmd
@@ -1,7 +1,11 @@
 @echo off
 
-cd ../src
+cd ..
+echo Running cppcheck ...
+call cppcheck src/main.c src/headers/*.h
+echo.
 
+cd src
 echo Running clang-format ...
 call "./../admin/tools/bin/clang-format" --verbose --style=file -i *.c headers/*.h
 echo Source code formatted.
