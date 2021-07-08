ba4edcf3d0ecaee2f39cd5b4037aaf7949448b87
Alex Schmitz <alexander.schmitz@uni-due.de>
2021-07-08 11:13:54 +0200

    Add example code for plugin.

---
+++ src/Example Plugin/headers/plugin.c
@@ -0,0 +1,14 @@
+/**
+ * This file is part of the OriginC Spectral Analysis package.
+ *
+ * (c) Alexander Schmitz <alexander.schmitz@uni-due.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+#include <Origin.h>
+
+void PLUGIN_HEADER_talk() {
+    // some output
+    printf("\n\nAnd I am a helper function in the Example Plugin header file.");
+}
---
+++ src/Example Plugin/headers/plugin.h
@@ -0,0 +1,21 @@
+/**
+ * This file is part of the OriginC Spectral Analysis package.
+ *
+ * (c) Alexander Schmitz <alexander.schmitz@uni-due.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+#ifndef _PLUGIN_HEADER_  // include once
+#define _PLUGIN_HEADER_
+
+/**
+ * This file provides an exmaple for a plugin header.
+ */
+
+/**
+ * Just output some text.
+ */
+void PLUGIN_HEADER_talk();
+
+#endif
